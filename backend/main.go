2743259115559342cec381dab307b84167eec5cf
fernandovmedina <correosfernandomedina@gmail.com>
2024-08-14 23:45:26 -0600

    database added to backend

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/fernandovmedina/netflix-clone-react/backend/src/database"
+)
+
+func main() {
+	var err error
+
+	if _, err = database.ConnDB(); err != nil {
+		log.Println(err.Error())
+	}
+
+	var router = http.NewServeMux()
+
+	var server = http.Server{
+		Addr:         ":8000",
+		Handler:      router,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	if err = server.ListenAndServe(); err != nil {
+		log.Println(err.Error())
+	}
+}
