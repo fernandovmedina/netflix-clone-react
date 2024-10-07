6186f9675a23523a1b93fdd19fe9b4e5e0465e83
fernandovmedina <correosfernandomedina@gmail.com>
2024-10-07 00:39:06 -0600

    continue with home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,31 @@
 import FooterHome from "../components/FooterHome";
 import NavHome from "../components/NavHome";
+import { movies, series } from "./series";
 
 const Home = () => {
+    const renderItems = (items: Record<string, string>, title: string) => (
+        <div className="pb-10">
+            <h1 className="text-xl mb-3">{title}</h1>
+            <div className="flex">
+                {Object.entries(items).map(([key, value]) => (
+                    <img className="px-1 w-72" key={key} src={value} />
+                ))}
+            </div>
+        </div>
+    );
+
     return (
         <main>
             <NavHome />
+
+            <section className="bg-black text-white px-16">
+                {renderItems(movies, "Movies")}
+                {renderItems(series, "Series")}
+            </section>
+
             <FooterHome />
         </main>
     );
-}
+};
 
 export default Home;
---
+++ src/pages/series.ts
@@ -0,0 +1,15 @@
+export const movies = {
+    godfather: "/backgrounds/AAAABaXG0_SkcxDP4NKIZuZM0A2jqVxn47Vij18mNPP1dVd5INlsGHtKKNeCVdZw8H--Lzpy57hlWE_MgG07ZN1gPjRzavFLIdN5s1A.webp",
+    supercool: "/backgrounds/AAAABTfukuVnAaPGDXMOUYZQHWdC0CFuMCIT5WELdG9OlWMPxgVFefqz7Zo6tPLLk0wVOdXVXMa3Kzku-ejyj4mJBTZUF26pU9-XTok.webp",
+    bloodGlor: "/backgrounds/AAAABVTji7h5Ats17_Js2m0JKc_T0vrA70ha-ykFLioPDNb254C0pYfh6rb5qIsXuQ5Qn3nz2RR-ldRvCMQ-0TVckb-qpSIIr3xb6U0.webp",
+    spiderman: "/backgrounds/AAAABX-d9BzkHIrtSMxdqdigQznXB62zr0Wh_162NBCWweTDH9I6QlJ0sHwc8ZeQvC2C2G-tZolWR3o9RHt7scdhvlHRlqJMDLSWJrI.webp",
+    fightClub: "/backgrounds/AAAABZSlPZqKP_kD1SgsG747piKcR8_SyolHmsgrBnk8zMalKv-cjzvG7LwT5PUsd6eJ6v0lZA_Nrjq4EpuHJEsa2_iyRLR19TpvPs8.webp"
+}
+
+export const series = {
+    deathNote: "/backgrounds/AAAABe9oSPxz1kvPl9s5GArm21QSuu-qhZ3IF-zAmQwSxgP5302PW8yGSrX0P_ywrU6RKxgeF7OiRFMG_th6WfAnh79qJjrytRh8zck.webp",
+    howToSellDrugsOnline: "/backgrounds/AAAABS7obUdqpvSCZXCVeJwfTr5w_3YHd-F36rD1sxxU-_9N9d-5vlM54nVEv53fP6DIfXfc-1ZFFmyb8IrHvEMIajAj6_N-flhJJFM_ZjEm_ANE-KdlwiGTrAmhG2YEIyG4txZn.jpg",
+    sexEducation: "/backgrounds/AAAABSlNBUVr977MUKhrldyzr1EKUiNnrCPzjG8Evt-fEgWO15SGgPxRhgJ7sXlg8LycNyXAm8gwBr-DCkCchdEbufPdf1jaBuVKjY93J2KVhGgnx-hKwAqi1dqUuUrDlQhW6_oF.jpg",
+    baki: "/backgrounds/AAAABXeYUgr_03NcXHzYWF46P-zqcRmJW42XxSJIDI4Faj3l0_qyWNnxyiIduq2xQqi0ffVFIzq9aUeAEkz5PQ8_KhqMg9af2HAv6dG5Kfs1aZtI8ge-fU4gLDvvzCLU_wEY-oyf.jpg",
+    breakingBad: "/backgrounds/AAAABZ_wyuaKDX2rC8U6CONyA1ZeHvlF8tcl3bTw7rcp4dnY9ziAEwIy_Gb6-cUO7khOgUB9sQoOr5YZcDqZ7JL0UZD2ImpFysUikCo.webp",
+}
